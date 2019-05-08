d67ecc6879d5947c340ce04a98569a2524488d33
Andrius Kirkyla <andriuskirkyla@Andriuss-MBP.fios-router.home>
2019-05-08 17:40:46 -0400

    Removed console.log code

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,4 @@
-console.log('JS Called')
 
-
-// fetch('http://localhost:3000/weather?address=boston').then((response) => {
-//     response.json().then((data) => {
-//         if(data.error){
-//             console.log(data.error)
-//         }
-//         else{
-//             console.log(data.location)
-//             console.log(data.forecast)
-//         }
-
-//     })
-// })
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
@@ -39,8 +25,7 @@ weatherForm.addEventListener('submit', (e) => {
             messageOne.textContent = data.location
             messageTwo.textContent = data.forecast
             
-            // console.log(data.location)
-            // console.log(data.forecast)
+        
         }
 
     })
